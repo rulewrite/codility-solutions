cf535704d44134b99ac06083c05839a89b4a405b
rulewrite <rulewrite@gmail.com>
2020-06-28 12:49:06 +0900

    fix: 이름이 뒤 바뀐 문제 파일명 변경

--- lessons/4-Counting-Elements/MaxCounters.js
+++ lessons/4-Counting-Elements/MaxCounters.js
@@ -1,24 +1,31 @@
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-function solution(A) {
+function solution(N, A) {
     // write your code in JavaScript (Node.js 8.9.4)
 
-    if (A.every((number) => 0 > number)) {
-        return 1;
-    }
+    let maxCount = 0;
+    let setMaxCount = 0;
+    return A.reduce((counts, countPosition) => {
+        const countIndex = countPosition - 1;
+        if (countIndex >= N) {
+            setMaxCount = maxCount;
+            return counts;
+        }
 
-    const numberArray = [];
-    A.forEach((leafX) => {
-        numberArray[leafX - 1] = true;
-    });
-
-    const missedIndex = numberArray.findIndex((flag) => !flag);
-    return (missedIndex > -1 ? missedIndex : numberArray.length) + 1;
+        const currentCount = counts[countIndex];
+        const nextCount =
+            (setMaxCount > currentCount ? setMaxCount : currentCount) + 1;
+        if (nextCount > maxCount) {
+            maxCount = nextCount;
+        }
+        counts[countIndex] = nextCount;
+        return counts;
+    }, new Array(N).fill(0)).map((count) =>
+        setMaxCount > count ? setMaxCount : count
+    );
 }
 
-// https://app.codility.com/demo/results/training2VA4MV-KQB/
-console.log(solution([1, 3, 6, 4, 1, 2, 7])); // 5
-console.log(solution([1, 2, 3])); // 4
-console.log(solution([-1, -3])); // 1
-console.log(solution([2, 3])); // 1
+// https://app.codility.com/demo/results/trainingQ87N2E-52W/
+console.log(solution(5, [3, 4, 4, 6, 1, 4, 4]));
+console.log(solution(1, [1]));
